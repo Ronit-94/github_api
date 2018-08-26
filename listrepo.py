55420fe1617e3435245922fa5802b6cfdc7fd48a
Ronit-94 <ronitgandhi94@gmail.com>
2018-08-26 14:03:51 -0400

    latest_update

--- listrepo.py
+++ listrepo.py
@@ -1,18 +1,13 @@
-from github import Github
 import sys
 import json
-import base64
 import requests
 
 def get_repositories(username, password):
 
     url = "https://api.github.com/user/repos"
-    #headers = {"Authorization": "Basic " + base64.urlsafe_b64encode("%s:%s" % (username, password)), "Content-type": "application/json"}
     r = json.loads(requests.get(url, auth = (username, password)).text)
     for repo in r:
         print(repo["name"])
-
-
 
 
 if __name__=="__main__":
