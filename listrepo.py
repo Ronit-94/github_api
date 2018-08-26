ccf9cb3dae1385ee7a221ff737cb304f7268a988
Ronit-94 <ronitgandhi94@gmail.com>
2018-08-26 14:30:14 -0400

    latest_update

--- listrepo.py
+++ listrepo.py
@@ -2,17 +2,20 @@ import sys
 import json
 import requests
 
-def get_repositories(username, password):
+token = "6fe21b93e82a0cca4e555689c872cca6fbd6f014"
+user = "Ronit-94"
 
-    url = "https://api.github.com/user/repos"
-    r = json.loads(requests.get(url, auth = (username, password)).text)
+def get_repositories(username):
+    url = "https://api.github.com/users/" + username + "/repos"
+    response = requests.get(url, auth = (user, token))
+    response_content = json.loads(response.text)
+
+    #listing all pubic repos associated with the username provided
     for repo in r:
         print(repo["name"])
 
 
 if __name__=="__main__":
     username = sys.argv[1]
-    password = sys.argv[2]
 
-
-    get_repositories(username, password)
+    get_repositories(username)
